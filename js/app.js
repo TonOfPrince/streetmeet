4407901dda1e681ba379871b286b8e9a37437807
Brett Leibowitz <LEIBO@Bretts-MacBook-Pro.local>
2015-04-18 17:49:40 -0400

    removed unnecessary files

--- js/app.js
+++ js/app.js
@@ -9,20 +9,11 @@ var meetApp = angular.module('sm-meetApp',
     'firebase',
     'ui.router',
     'ngCookies',
-    'angular-gestures',
     'sm-meetApp.login',
-    'sm-meetApp.createEvents',
-    'sm-meetApp.joinEvent',
     'sm-meetApp.userInterfaceController',
     'sm-meetApp.currentUser',
-    // 'sm-meetApp.event',
-    'sm-meetApp.profile',
-    'sm-meetApp.editEvent',
-    'sm-meetApp.profileSettings',
     'sm-meetApp.oneMap',
-    'sm-meetApp.sendLoc',
     'sm-meetApp.vertDrag'
-
   ])
 
 // set up routing
@@ -35,49 +26,10 @@ var meetApp = angular.module('sm-meetApp',
       templateUrl : 'views/login.html',
       controller: 'LoginCtrl'
     })
-    .state('createEvent', {
-      url: '/createEvent',
-      templateUrl: 'views/createEventForm.html',
-      controller: 'CreateEventsCtrl'
-    })
-    .state('listCurrentEvents', {
-      url: '/listEvents',
-      templateUrl: 'views/listCurrentEvents.html',
-      controller: 'JoinEventCtrl'
-    })
-    // .state('attendEvent', {
-    //   url: '/event/:id',
-    //   templateUrl: 'views/attendEvent.html',
-    //   controller: 'EventCtrl'
-    // })
-    .state('settings', {
-      url: '/settings',
-      templateUrl: 'views/userSettings.html',
-    })
-    .state('userProfile', {
-      url: '/userProfile/:id',
-      templateUrl: 'views/userProfile.html',
-      controller: 'SettingsCtrl'
-    })
     .state('map', {
       url: '/map',
       templateUrl: 'views/map.html',
       controller: 'OneMapCtrl'
-    })
-    .state('userProfileSettings', {
-      url: '/userProfileSettings/:id',
-      templateUrl: 'views/userProfileSettings.html',
-      controller: 'ProfileSettingsCtrl'
-    })
-    .state('editEventSettings', {
-      url: '/editEvent/:id',
-      templateUrl: 'views/editEvent.html',
-      controller: 'EditEventCtrl'
-    })
-    .state('sendLoc', {
-      url: '/sendLoc',
-      templateUrl: 'views/sendLoc.html',
-      controller: 'SendLocCtrl'
     })
 });
 
--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-angular.module('sm-meetApp.login',  ['firebase', 'ngCookies', 'angular-gestures'])
+angular.module('sm-meetApp.login',  ['firebase', 'ngCookies'])
 
 
 
