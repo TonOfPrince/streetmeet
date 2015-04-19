47494809d5aa10e48ab4660481bb5b0fbd76024b
Brett Leibowitz <LEIBO@Bretts-MacBook-Pro.local>
2015-04-19 06:01:03 -0400

    fixed rest of htabs while still being draggable

--- js/vertDrag.js
+++ js/vertDrag.js
@@ -3,15 +3,9 @@ directive('draggable', function($document) {
   return function(scope, element, attr) {
     var startY = 0, y = 0, offsetHeight = element[0].offsetHeight,
         maxHeight = offsetHeight + element[0].offsetTop;
-    console.log(element);
-    element.css({
-     // position: 'relative',
-    });
     element.on('touchstart', function(event) {
-      console.log(event);
     // element.on('mousedown', function(event) {
       // Prevent default dragging of selected content
-      event.preventDefault();
       startY = event.touches[0].screenY - y;
       // $document.on('mousemove', mousemove);
       $document.on('touchmove', touchmove);
@@ -20,8 +14,8 @@ directive('draggable', function($document) {
     });
 
     function touchmove(event) {
+      event.preventDefault();
       y = event.touches[0].screenY - startY;
-      console.log(y);
       if (y < maxHeight*(-1)) {
         y  = maxHeight*(-1);
         element.css({
